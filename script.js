27fed739c369550a39423de05175e3744abc1293
Raviraj Dulange <redhood.dumb@gmail.com>
2021-05-03 19:32:57 +0530

    Add complete files

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-const icons = document.querySelectorAll(".section1-icons i");
-let counter = 1;
+document.querySelector('.menu').addEventListener('click', () => {
+	document.querySelectorAll('.target').forEach((item) => {
+		item.classList.toggle('change');
+	})
+})
+
+const icons = document.querySelectorAll('.section-1-icons i')
+let i = 1
+
 setInterval(() => {
-  counter++;
-  const icon = document.querySelector(".section1-icons .change");
-  icon.classList.remove("change");
-  if (counter > icons.length) {
-    icons[0].classList.add("change");
-    counter = 1;
-  } else {
-    icon.nextElementSibling.classList.add("change");
-  }
-}, 4000);
+	i++
+	const icon = document.querySelector('.section-1-icons .change')
+	icon.classList.remove('change')
+
+	if (i > icons.length) {
+		icons[0].classList.add('change')
+		i = 1
+	} else {
+		icon.nextElementSibling.classList.add('change')
+	}
+}, 4000)
